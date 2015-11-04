a9195e3c82d39269002fdac964c9a9da8dec7002
shota992277 <shota.oda@timedesign.co.jp>
2015-11-04 16:55:18 +0900

    a

---
+++ MvcApp2/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApp2.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index(string returnUrl)
+		{
+			//testaaa
+			ViewBag.ReturnUrl = returnUrl;
+			return View();
+		}
+	}
+}
